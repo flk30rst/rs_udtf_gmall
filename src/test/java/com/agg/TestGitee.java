1e1e5a1679f8b480b6a82b4fc1dbfb6986c02d77
flk30rst <flk30rst@163.com>
2021-04-27 20:38:19 +0800

    readme commit

---
+++ src/test/java/com/agg/TestGitee.java
@@ -0,0 +1,12 @@
+package com.agg;
+
+/**
+ * @author wanzhengyu
+ * @date 2021/4/27 19:29
+ */
+public class TestGitee {
+    public static void main(String[] args) {
+        System.out.println("gitee");
+    }
+}
+
