5738c8145511ba52555a443c64cea73e778589c8
flk30rst <flk30rst@163.com>
2021-04-27 15:32:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/gmall/ExplodeJSONArray.java
@@ -0,0 +1,58 @@
+package com.atguigu.gmall;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.json.JSONArray;
+import java.util.ArrayList;
+
+public class ExplodeJSONArray extends GenericUDTF {
+    @Override
+    public StructObjectInspector initialize(ObjectInspector[] argOIs) throws UDFArgumentException {
+        // 1 参数合法性检查
+        if (argOIs.length != 1) {
+            throw new UDFArgumentException("ExplodeJSONArray 只需要一个参数");
+        }
+
+        // 2 第一个参数必须为string
+        if (!"string".equals(argOIs[0].getTypeName())) {
+            throw new UDFArgumentException("json_array_to_struct_array的第1个参数应为string类型");
+        }
+
+        // 3 定义返回值名称和类型
+        ArrayList<String> fieldNames = new ArrayList<String>();
+        ArrayList<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
+
+        fieldNames.add("item");
+        fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+
+        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
+    }
+
+    @Override
+    public void process(Object[] args) throws HiveException {
+        // 1 获取传入的数据
+        String jsonArray = args[0].toString();
+
+        // 2 将string转换为json数组
+        JSONArray actions = new JSONArray(jsonArray);
+
+        // 3 循环一次，取出数组中的一个json，并写出
+        for (int i = 0; i < actions.length(); i++) {
+            String[] result = new String[1];
+            result[0] = actions.getString(i);
+            forward(result);
+        }
+    }
+
+    @Override
+    public void close() throws HiveException {
+        // show functions like "explode*";
+        // netstat -anp | grep:10000
+    }
+
+}
---
+++ src/test/java/com/agg/TestGit.java
@@ -0,0 +1,7 @@
+package com.agg;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
